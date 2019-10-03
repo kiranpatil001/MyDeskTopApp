e93c5f5780da7a8a730685789230c9e9c6b368a4
kiran patil <kiran.patil@siemens.com>
2019-10-03 23:20:15 +0530

    commit 8

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi Kiran ... I am here");
+            Console.WriteLine("Hi Kiran Program... I am here");
         }
     }
 }
